7d5d4970ef99c6a57a7e07ed9b929075b50caaec
Mohd Shadab <mohdshadab@Mohds-MacBook-Air.local>
2021-10-31 14:49:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk #tk is a class in tkinter
+from tkinter import ttk
+from tkinter import font,colorchooser,filedialog,messagebox
+import os
+
+main_application=tk.Tk() 
+#tk is alias of tkinter 
+# main_application is object
+#  Tk is class in tkinter
+main_application.geometry('1200x800')
+main_application.title('Text Editor')
+
+####################### main menu ###############################
+
+######################tool bar ##################################
+
+######################text editor###############################
+
+#######################status bar###############################
+
+########################shortcut keys###########################
+
+main_application.mainloop()
